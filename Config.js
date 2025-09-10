8cf2f0c698ee6086f1dc70bd11b088838dd54647
sukanyaphbudc-lang <sukanya.ph.budc@cpird.in.th>
2025-09-10 16:33:12 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,9 +1,10 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbzvhDgmnqlP1VosYHUWU1CrBiUq_E-1qZHRD2c_HlPih3_TOhg6o1csmgXsV4xSQdl4tg/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwkJP2Q6wamsKK0X76SfF3wFBDIgHrRiO8ZMgFwJcC2IUX0nDNoR2zznsytbx4RD2Rcfg/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwuEGEm6Gq4hWoMF9W0fpbb4qcYYpQK1inaHDEBNb1tJYA9DlNRc-RHVGrQN_HrIjjz-w/exec';
 //LIFF_ID
     const LIFF_ID = '2008087987-B65jE33P'; // LIFF ID Checkin
     const LIFF_HISTORY = '2008087987-m6POlRRx'; // LIFF ID history
     const LIFF_REGISTER = '2008087987-lxdeMxxg'; // LIFF ID register
+
 
 
