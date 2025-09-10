dda8a7c0cb8e098d1cc1aeb5afe70a68065720a8
sukanyaphbudc-lang <sukanya.ph.budc@cpird.in.th>
2025-09-10 15:25:20 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,6 +2,7 @@
     const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwR5z1QhMd3588Hn5pW7kF55p0pUp4AG2ZmbzVDHTOhNBOFjjPuWdKIny8G8_rT0rodmA/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxu9h0ZJLm0h__BEahkGehV5nahKBs-aED6V4hYQnw-XxBmoUEuxIyZ4YHr_CRXEm53/exec';
 //LIFF_ID
-    const LIFF_ID = '2007747842-OGeyrGkv'; // LIFF ID Checkin
-    const LIFF_HISTORY = '2007747842-p0YX1oBn'; // LIFF ID history
-    const LIFF_REGISTER = '2007747842-oJr1GXK2'; // LIFF ID register
+    const LIFF_ID = '2008087987-B65jE33P'; // LIFF ID Checkin
+    const LIFF_HISTORY = '2008087987-m6POlRRx'; // LIFF ID history
+    const LIFF_REGISTER = '2008087987-lxdeMxxg'; // LIFF ID register
+
