f2191b6a88eb2d923dcaf78df5130a8d2d0aca51
sukanyaphbudc-lang <sukanya.ph.budc@cpird.in.th>
2025-09-10 15:59:52 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,8 +1,9 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwR5z1QhMd3588Hn5pW7kF55p0pUp4AG2ZmbzVDHTOhNBOFjjPuWdKIny8G8_rT0rodmA/exec';
-    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxu9h0ZJLm0h__BEahkGehV5nahKBs-aED6V4hYQnw-XxBmoUEuxIyZ4YHr_CRXEm53/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbzvhDgmnqlP1VosYHUWU1CrBiUq_E-1qZHRD2c_HlPih3_TOhg6o1csmgXsV4xSQdl4tg/exec';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwuEGEm6Gq4hWoMF9W0fpbb4qcYYpQK1inaHDEBNb1tJYA9DlNRc-RHVGrQN_HrIjjz-w/exec';
 //LIFF_ID
     const LIFF_ID = '2008087987-B65jE33P'; // LIFF ID Checkin
     const LIFF_HISTORY = '2008087987-m6POlRRx'; // LIFF ID history
     const LIFF_REGISTER = '2008087987-lxdeMxxg'; // LIFF ID register
+
 
